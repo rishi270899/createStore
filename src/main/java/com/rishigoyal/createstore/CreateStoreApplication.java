59c1f4c69b120210f042daea8183f1c020c58e32
Rishi Goyal <rishig516@gmail.com>
2026-05-26 20:16:21 +0530

    springboot project setup

---
+++ src/main/java/com/rishigoyal/createstore/CreateStoreApplication.java
@@ -0,0 +1,28 @@
+package com.rishigoyal.createstore;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateStoreApplication {
+
+    public static void main(String[] args) {
+
+        // configuration of dotenv-java
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        dotenv
+                .entries()
+                .forEach((DotenvEntry entry) ->
+                        System.setProperty(entry.getKey(), entry.getValue())
+                );
+
+        SpringApplication.run(CreateStoreApplication.class, args);
+
+
+
+    }
+
+}
---
+++ src/test/java/com/rishigoyal/createstore/CreateStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishigoyal.createstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
