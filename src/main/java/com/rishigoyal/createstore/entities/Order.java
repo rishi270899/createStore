3a330a5fd34065a767cbd3049afb50e74081d702
Rishi Goyal <rishig516@gmail.com>
2026-05-26 21:00:21 +0530

    order entity class add

---
+++ src/main/java/com/rishigoyal/createstore/entities/Order.java
@@ -0,0 +1,46 @@
+package com.rishigoyal.createstore.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "customer_name")
+    private String customerName;
+
+    @Column(name = "customer_email", nullable = false)
+    private String customerEmail;
+
+    @Column(nullable = false)
+    private String Status;
+
+    @Column(name = "total_price", nullable = false)
+    private BigDecimal totalPrice;
+
+    @Column(name= "created_at")
+    private LocalDateTime createdAt;
+//    private LocalDateTime updatedAt;
+
+
+    public void prePersist(){
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
